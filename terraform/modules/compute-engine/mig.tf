9b08f31692eb528f6709439b8f172e97ad30e30f
TATSUYA KUROKAWA <thepekinensis@gmail.com>
2025-06-16 05:45:42 +0900

    fix

--- terraform/modules/compute-engine/mig.tf
+++ terraform/modules/compute-engine/mig.tf
@@ -105,8 +105,8 @@ resource "google_compute_region_instance_group_manager" "dify" {
     type                           = "PROACTIVE"
     minimal_action                 = "REPLACE"
     most_disruptive_allowed_action = "REPLACE"
-    max_surge_fixed                = 0
-    max_unavailable_fixed          = 1
+    max_surge_fixed                = 3  # リージョナルMIGでは最低でもゾーン数が必要（通常3ゾーン）
+    max_unavailable_fixed          = 0  # ダウンタイムなし
     replacement_method             = "SUBSTITUTE"
   }
 
